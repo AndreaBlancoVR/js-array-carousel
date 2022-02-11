94d26a21213a5e00dfe349c5ffa9f793682b1fed
andreablancovr@gmail.com <andreablancovr@gmail.com>
2022-02-11 02:13:28 +0100

    Ho fatto inchiodare il browser. Riavvio pc

--- app.js
+++ app.js
@@ -35,38 +35,71 @@ MAIN_WRAPPER.classList.add('main-wrapper');
 ROW_MASTER.append(MAIN_WRAPPER);
 
 const MAIN_ITEM = document.createElement('div');
-MAIN_ITEM.classList.add('main-item')
-MAIN_WRAPPER.append(MAIN_ITEM)
+MAIN_ITEM.classList.add('main-item');
+MAIN_WRAPPER.append(MAIN_ITEM);
+
+const MAIN_IMG = document.createElement('img');
+MAIN_ITEM.append(MAIN_IMG);
 
 const MAIN_TEXT = document.createElement('div');
-MAIN_TEXT.classList.add('main-text')
-MAIN_WRAPPER.append(MAIN_TEXT)
+MAIN_TEXT.classList.add('main-text');
+MAIN_WRAPPER.append(MAIN_TEXT);
 
-const TITLE = document.createElement('h3');
-MAIN_TEXT.append(TITLE)
+let IMG_TITLE = document.createElement('h3');
+MAIN_TEXT.append(IMG_TITLE);
 
-const CAPTION = document.createElement('p');
-MAIN_TEXT.append(CAPTION)
+let IMG_CAPTION = document.createElement('p');
+MAIN_TEXT.append(IMG_CAPTION);
 
 const SIDE_BAR = document.createElement('aside');
-SIDE_BAR.classList.add('side-bar')
-ROW_MASTER.append(SIDE_BAR)
+SIDE_BAR.classList.add('side-bar');
+ROW_MASTER.append(SIDE_BAR);
 
 const SIDE_WRAPPER = document.createElement('div');
-SIDE_WRAPPER.classList.add('side-wrapper')
-SIDE_BAR.append(SIDE_WRAPPER)
+SIDE_WRAPPER.classList.add('side-wrapper');
+SIDE_BAR.append(SIDE_WRAPPER);
 
 const UL = document.createElement('ul');
-SIDE_WRAPPER.append(UL)
+SIDE_WRAPPER.append(UL);
 
 const ARROWS = document.createElement('div');
-ARROWS.classList.add('arrows')
-SIDE_WRAPPER.append(ARROWS)
+ARROWS.classList.add('arrows');
+SIDE_WRAPPER.append(ARROWS);
 
 const UP_ARROW = document.createElement('span');
-UP_ARROW.classList.add('up-arrow')
-ARROWS.append(UP_ARROW)
+UP_ARROW.classList.add('up-arrow');
+UP_ARROW.innerHTML = ('SU')
+ARROWS.append(UP_ARROW);
 
 const DOWN_ARROW = document.createElement('span');
-UP_ARROW.classList.add('down-arrow')
-ARROWS.append(DOWN_ARROW)
+DOWN_ARROW.classList.add('down-arrow');
+DOWN_ARROW.innerHTML = ('GIU\'')
+ARROWS.append(DOWN_ARROW);
+
+let currentIndex = 0
+
+
+for(let i = 0; i < items.length; i++) {
+    const liElement = document.createElement('li');
+    UL.append(liElement);
+
+    let overlay = document.createElement('div')
+    overlay.classList.add('item-overlay');
+    
+    const imgContElement = document.createElement('div');
+    liElement.append(imgContElement);
+
+    if(i > 0 ) {
+        liElement.append(overlay);
+    }
+
+    
+
+    const imgElement = document.createElement('img');
+    imgContElement.append(imgElement);
+    imgElement.src = items[i];
+
+    MAIN_IMG.src = items[0];
+    IMG_TITLE.append(title[0]);
+    IMG_CAPTION.append(text[0]);  
+}
